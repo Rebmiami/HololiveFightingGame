814c984aef29547c8c0cbda21aa3a106225c2e86
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2021-04-02 18:16:01 -0400

    Added MoveRunner to projectile

--- Combat/Projectile.cs
+++ Combat/Projectile.cs
@@ -4,24 +4,26 @@ using System.Text;
 
 namespace HololiveFightingGame.Combat
 {
-    public class Projectile : Entity
-    {
-        public int owner;
-        public int timeLeft;
+	public class Projectile : Entity
+	{
+		public int owner;
+		public int timeLeft;
 
-        public static Projectile Add()
-        {
-            // TODO: Use this method to add a projectile to the list in GameState
-            // Projectile.Add(...); is cleaner than Game1.gameState.projectiles.Add(new Projectile(...));
-            Projectile newProjectile = new Projectile();
-            Game1.gameState.projectiles.Add(newProjectile);
-            return newProjectile;
-        }
+		public MoveRunner moveRunner;
 
-        public override void Update()
-        {
-            timeLeft--;
-            base.Update();
-        }
-    }
+		public static Projectile Add()
+		{
+			// TODO: Use this method to add a projectile to the list in GameState
+			// Projectile.Add(...); is cleaner than Game1.gameState.projectiles.Add(new Projectile(...));
+			Projectile newProjectile = new Projectile();
+			Game1.gameState.projectiles.Add(newProjectile);
+			return newProjectile;
+		}
+
+		public override void Update()
+		{
+			timeLeft--;
+			base.Update();
+		}
+	}
 }
