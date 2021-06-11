11dd1078e9a169b1b0facbbe633159cf8fd912b8
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2021-06-11 00:11:35 -0400

    Coded an icon artist to draw all the icons

---
+++ FighterEditor/IconArtist.cs
@@ -0,0 +1,25 @@
+﻿using HololiveFightingGame.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HololiveFightingGame.FighterEditor
+{
+	public static class IconArtist
+	{
+		public static Texture2D editorIcons;
+
+		public static void DrawIcon(SpriteBatch spriteBatch, Vector2 position, EditorIcon icon)
+		{
+			Point iconPositionOnSheet = new Point((int)icon % 12 * 25, (int)icon / 12 * 25);
+			spriteBatch.Draw(editorIcons, position, new Rectangle(iconPositionOnSheet, new Point(24, 24)), Color.White);
+		}
+
+		static IconArtist()
+		{
+			editorIcons = ImageLoader.LoadTexture(@".\Content\Assets\EditorIcons.png", true);
+		}
+	}
+}
